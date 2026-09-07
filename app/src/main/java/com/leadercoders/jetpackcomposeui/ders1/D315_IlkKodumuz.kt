e0e369c9f3eb608062b2c005db5e33b471752f38
Ahmet Baran Çelik <ahmetbarancelik81@gmail.com>
2026-09-07 12:52:27 +0300

    Ders1- Proje Oluşturuldu

---
+++ app/src/main/java/com/leadercoders/jetpackcomposeui/ders1/D315_IlkKodumuz.kt
@@ -0,0 +1,9 @@
+package com.leadercoders.jetpackcomposeui.ders1
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SelamlamaEkrani(){
+    Text("Merhaba Dünya! İlk Compose uygulamam çalışıyor 🎉")
+}
